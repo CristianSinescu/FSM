be3f2ae1fdc244db643d0473f0a12b7eef483f8a
Sinescu Cristian <sinescucristian@gmail.com>
2017-11-17 04:22:51 +0200

    FSM done

---
+++ FSM/FSM/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> d =
+                new Dictionary<string, Dictionary<string, string>>()
+                {
+                    {"0", new Dictionary<string, string>() {{"5", "5"}, {"10", "10"}, {"0", "0"}}},
+                    {"5", new Dictionary<string, string>() {{"5", "10"}, {"10", "15"}}},
+                    {"10", new Dictionary<string, string>() {{"5", "15"}, {"C10", "0"}}},
+                    {"15", new Dictionary<string, string>() {{"C10", "5"}, {"C15", "0"}}}
+
+                };
+            string action = string.Empty;
+            int choose = 0;
+            Console.WriteLine("Do you want to start the coffe machine?");
+            Console.WriteLine("Press 1 for start and 0 for not");
+            choose = Convert.ToInt32(Console.ReadLine());
+            while (choose != 0)
+            {
+                switch (choose)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Insert action!");
+                            action = Convert.ToString(Console.ReadLine());
+                            CoffeMachine cf = new CoffeMachine(d, action);
+                            cf.execute();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Your choice isn't valid");
+                            break;
+                        }
+
+                }
+                Console.WriteLine("Press 1 to continue or 0 to exit");
+                choose = Convert.ToInt32(Console.ReadLine());
+
+            }
+        }
+    }
+}
---
+++ FSM/FSM/StateMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSM
+{
+    class StateMachine
+    {
+        protected static string currentState="0";
+        protected string action;
+        protected Dictionary<string, Dictionary<string, string>> table;
+
+        protected StateMachine( string action)
+        {
+            this.action = action;
+        }
+        public void execute()
+        {
+            Console.WriteLine("The initial state is "+currentState);
+            Dictionary<string, string> tmpDictionary = new Dictionary<string, string>();
+            bool a = table.TryGetValue(currentState, out tmpDictionary);
+            if (a == false)
+            {
+                Console.WriteLine("Initial state isn't valid");
+                Console.WriteLine("Please try again!");
+                return;
+            }
+            bool b = tmpDictionary.TryGetValue(action, out currentState);
+            if (b == false)
+            {
+                Console.WriteLine("Action isn't valid");
+                Console.WriteLine("Please try again!");
+                return;
+            }
+            if (String.Equals(action, "C10")|| String.Equals(action, "C15"))
+            {
+                Console.WriteLine("Your coffe is ready!");
+            }
+            Console.WriteLine("The current state is "+currentState);
+        }
+
+    }
+}
